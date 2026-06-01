68e86a0efd1b4b9050b76f70cc2819fde2e6cc78
Lalitha-Kalle <lalithaprogramming@gmail.com>
2026-06-01 15:28:11 +0530

    added all

--- app.py
+++ app.py
@@ -242,4 +242,5 @@ def get_problem(year, version, question_num):
 
 
 if __name__ == '__main__':
-    app.run(debug=False, port=int(os.getenv('PORT', 5000)))
+    port = int(os.getenv('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
